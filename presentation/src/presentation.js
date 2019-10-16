b6ab1debf5bd5b9699b4f4a37fe2f3c369aced41
Joe Purnell <joepurnell1@gmail.com>
2019-10-16 08:57:49 +0100

    Atomic Design in Presentation

--- presentation/src/presentation.js
+++ presentation/src/presentation.js
@@ -1,16 +1,21 @@
 // Import React
 import React from 'react';
-
+import { Play } from 'stylish/atoms/mediabutton';
 import {
   BlockQuote,
   Cite,
+  CodePane,
   Deck,
+  Fill,
+  Fit,
   Heading,
+  Image,
   ListItem,
   List,
   Quote,
   Slide,
   Text,
+  Layout,
 } from 'spectacle';
 import createTheme from 'spectacle/lib/themes/default';
 
@@ -18,16 +23,23 @@ require('normalize.css');
 
 const theme = createTheme(
   {
-    primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
-    quaternary: '#CECECE',
+    primary: '#02182B',
+    secondary: '#E84855',
+    tertiary: '#FFF',
+    quaternary: '#16F4D0',
   },
   {
     primary: 'Montserrat',
     secondary: 'Helvetica',
   }
 );
+
+const images = {
+  atomicDesign: require('./assets/atomic-design.png'),
+  atom: require('./assets/PlayButton.png'),
+  molecule: require('./assets/MusicControls.png'),
+  organism: require('./assets/MusicPlayer.png'),
+};
 
 export default class Presentation extends React.Component {
   render() {
@@ -43,46 +55,87 @@ export default class Presentation extends React.Component {
             Atomic Development
           </Heading>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
+
+        <Slide transition={['fade']} bgColor="secondary">
+          <Heading size={3} fit caps lineHeight={1} textColor="primary">
+            I'm Joe 🙋‍♂️
           </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
+          <Heading size={3} fit caps lineHeight={1} textColor="primary">
+            Digital Support Experience
           </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+
+        <Slide transition={['fade']} bgColor="primary">
+          <Heading size={3} fit caps lineHeight={1} textColor="secondary">
+            Atomic Design
+          </Heading>
+          <Heading size={3} fit caps lineHeight={1} textColor="tertiary">
+            (that Brad Frost design system methodology)
+          </Heading>
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="secondary">
+          <Heading size={3} fit caps lineHeight={1} textColor="primary">
+            Share UI code
+          </Heading>
+          <Heading size={3} fit caps lineHeight={1} textColor="primary">
+            across platforms
+          </Heading>
+        </Slide>
+
+        <Slide transition={['zoom']} bgColor="primary">
+          <Heading size={3} textColor="tertiary" caps>
+            Atomic Design
+          </Heading>
           <Heading size={6} textColor="secondary" caps>
-            Standard List
+            Brad Frost
           </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
         </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+
+        <Slide transition={['fade']} bgColor="primary" align="center center">
+          <Heading size={6} textColor="tertiary" caps fit>
+            Atomic Design
+          </Heading>
+          <Image src={images.atomicDesign.replace('/', '')} margin="0px auto 40px" />
         </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" align="center center">
+          <Heading size={3} textColor="tertiary" caps lineHeight={1}>
+            Atoms
+          </Heading>
+          <Image src={images.atom.replace('/', '')} margin="40px auto 0" height="200px" />
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" align="center center">
+          <Heading size={3} textColor="tertiary" caps lineHeight={1}>
+            Molecules
+          </Heading>
+          <Image src={images.molecule.replace('/', '')} margin="40px auto 0" height="200px" />
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" align="center center">
+          <Heading size={3} textColor="tertiary" caps lineHeight={1}>
+            Organisms
+          </Heading>
+          <Image src={images.organism.replace('/', '')} margin="40px auto 0" height="200px" />
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" align="center center">
+          <Layout>
+            <Fill>
+              <Heading size={3} textColor="tertiary" caps lineHeight={1}>
+                Templates
+              </Heading>
+            </Fill>
+            <Fill>
+              <Heading size={3} textColor="tertiary" caps lineHeight={1}>
+                Pages
+              </Heading>
+            </Fill>
+          </Layout>
+        </Slide>
+
+
       </Deck>
     );
   }
